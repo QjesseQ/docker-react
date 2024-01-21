ded39dab40da6dda15f42a87f177401e1497bcb6
Jesse Qiu <delongqiu@gmail.com>
2024-01-20 21:37:25 -0500

    changes for feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React i was changed
         </a>
       </header>
     </div>
